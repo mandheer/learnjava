1c65d5dc01744bc77bd13738b153ad25e59ee476
Mandheer <mandheer@pabreja.in>
2015-12-30 19:31:49 +0530

    update samples
    Signed-off-by: Mandheer <mandheer@pabreja.in>

--- common/src/main/java/com/learning/log4j/main/MyApp.java
+++ common/src/main/java/com/learning/log4j/main/MyApp.java
@@ -5,7 +5,7 @@ package com.learning.log4j.main;
 
 /**
  * @author mandheer
- *        
+ *         
  */
 public class MyApp {
 	
@@ -13,7 +13,8 @@ public class MyApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	
+		int i = -5;
+		System.out.println(i);
 	}
 	
 }
